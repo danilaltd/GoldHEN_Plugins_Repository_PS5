1358cdecca9cbe705ee766156a64e6b46bb1e89e
danilaltd <danilaltd2006@gmail.com>
2026-01-21 16:30:43 +0300

    fliprate_remover ported

--- plugin_src/fliprate_remover/source/main.c
+++ plugin_src/fliprate_remover/source/main.c
@@ -6,13 +6,15 @@
 // Repository: https://github.com/GoldHEN/GoldHEN_Plugins_Repository
 
 #include <Common.h>
-#include <orbis/VideoOut.h>
+// #include s<orbis/VideoOut.h>
 #include "plugin_common.h"
 
 attr_public const char *g_pluginName = "fliprate_remover";
 attr_public const char *g_pluginDesc = "Removes framerate limiter using sceVideoOutSetFlipRate";
 attr_public const char *g_pluginAuth = "illusion";
 attr_public u32 g_pluginVersion = 0x00000100; // 1.00
+
+int32_t sceVideoOutSetFlipRate(int32_t handle, int32_t fliprate);
 
 HOOK_INIT(sceVideoOutSetFlipRate);
 
@@ -42,4 +44,8 @@ s32 attr_module_hidden module_start(s64 argc, const void *args)
 s32 attr_module_hidden module_stop(s64 argc, const void *args)
 {
     return 0;
+}
+
+int main(int32_t argc, const char **argv) {
+    return(plugin_load(argc, argv));
 }
